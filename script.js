f9c6ad18d48279e89c5594e61fe5715dfab78ec7
Andrej Linux <ane.lazic@gmail.com>
2023-06-21 14:29:15 +0200

    light/dark theme

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 $(document).ready(function() {
-    var languageSwitch = $('#languageSwitch');
-    var language = 'english';
+    var languageSwitch = $("#languageSwitch");
+    var language = "english";
 
     languageSwitch.change(function() {
-        if (language === 'english') {
-            language = 'serbian';
+        if (language === "english") {
+            language = "serbian";
             $(".eng").hide();
             $(".srb").show();
           } else {
-            language = 'english';
+            language = "english";
             $(".srb").hide();
             $(".eng").show();
           }
     });
 });
+
+$(document).ready(function() {
+    var themeToggle = $('#themeToggle');
+    var body = $('body');
+    var currentTheme = 'light-theme';
+  
+    themeToggle.click(function() {
+      if (currentTheme === 'light-theme') {
+        currentTheme = 'dark-theme';
+        body.removeClass('light-theme').addClass('dark-theme');
+      } else {
+        currentTheme = 'light-theme';
+        body.removeClass('dark-theme').addClass('light-theme');
+      }
+    });
+  });
